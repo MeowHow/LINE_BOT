968506677aac313dcce96da2f0d02e8fd2759142
MeowHow <sss.www201011@gmail.com>
2019-12-16 11:59:24 +0800

    commit

--- msglist.py
+++ msglist.py
@@ -5,6 +5,8 @@ from linebot.models import *
 def replay_msglist(msg):
     if '你好' in msg:
         return "你好"
-    elif '' in mag 
+    elif '馬告' in msg and '功效' in msg:
+        return "馬告具有安眠、鎮痛與抗憂的功效喲～"
+
 
 
