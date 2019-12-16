489e636e8ab76c2abe25f9d6aaa68a0341197b79
MeowHow <sss.www201011@gmail.com>
2019-12-16 11:05:12 +0800

    commit

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
     else:
         #message = TextSendMessage(text=msg)
-        message = replay_msglist(msg)
+        message = "你好"
         line_bot_api.reply_message(event.reply_token, message)
 
 
