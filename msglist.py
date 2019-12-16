02de62813b42d22f2948e6f1f0cc6b245044d63f
MeowHow <sss.www201011@gmail.com>
2019-12-16 12:05:45 +0800

    commit

--- msglist.py
+++ msglist.py
@@ -2,13 +2,16 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
-def replay_msglist(msg):
-    if '你好' in msg:
-        return TextSendMessage(text="你好")
-    elif '馬告' in msg and '功效' in msg:
-        return TextSendMessage(text="馬告具有安眠、鎮痛與抗憂的功效喲～")
-    elif msg == "幹":
-    return TextSendMessage(text="幹三小？")
+def replay_msglist(T):
+    if '你好' in T:
+        msg = TextSendMessage(text="你好")
+        return msg
+    elif '馬告' in T and '功效' in T:
+        msg = TextSendMessage(text="馬告具有安眠、鎮痛與抗憂的功效喲～")
+        return msg
+    elif T == "幹":
+        msg = TextSendMessage(text="幹三小？")
+        return msg
 
 
 
