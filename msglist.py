92d7125af194daecdf99617cbc9943fc843e9ae3
MeowHow <sss.www201011@gmail.com>
2019-12-21 21:14:49 +0800

    commit

--- msglist.py
+++ msglist.py
@@ -13,49 +13,25 @@ def replay_msglist(T):
     elif T == "幹":
         msg = TextSendMessage(text="幹三小？")
         return msg
-        
+
 #店家
 def msglist_Store():
-    message = ImagemapSendMessage(
-        base_url="https://i.imgur.com/BfTFVDN.jpg",
+    message = TemplateSendMessage(
         alt_text='店家',
-        base_size=BaseSize(height=2000, width=2000),
-        actions=[
-            URIImagemapAction(
-                link_uri="https://qulaw123.myorganic.org.tw/",
-                area=ImagemapArea(
-                    x=0, y=0, width=1000, height=1000
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    title='烏來泰雅農產合作社',
+                    text='請按連結',
+                    actions=[
+                        URITemplateAction(
+                            label='合作社',
+                            uri='https://qulaw123.myorganic.org.tw/'
+                        )
+                    ]
                 )
-            ),
-            URIImagemapAction(
-                #生活市集
-                link_uri="https://tw.shop.com/search/%E7%94%9F%E6%B4%BB%E5%B8%82%E9%9B%86",
-                area=ImagemapArea(
-                    x=1000, y=0, width=1000, height=1000
-                )
-            ),
-            URIImagemapAction(
-                #阿瘦皮鞋
-                link_uri="https://tw.shop.com/search/%E9%98%BF%E7%98%A6%E7%9A%AE%E9%9E%8B",
-                area=ImagemapArea(
-                    x=0, y=1000, width=1000, height=1000
-                )
-            ),
-            URIImagemapAction(
-                #塔吉特千層蛋糕
-                link_uri="https://tw.shop.com/search/%E5%A1%94%E5%90%89%E7%89%B9",
-                area=ImagemapArea(
-                    x=1000, y=1000, width=1000, height=500
-                )
-            ),
-            URIImagemapAction(
-                #亞尼克生乳捲
-                link_uri="https://tw.shop.com/search/%E4%BA%9E%E5%B0%BC%E5%85%8B",
-                area=ImagemapArea(
-                    x=1000, y=1500, width=1000, height=500
-                )
-            )
-        ]
+            ]
+        )
     )
     return message
 
