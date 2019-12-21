fe83b9946c8f1541762419c39e44da4a43e5b291
MeowHow <sss.www201011@gmail.com>
2019-12-21 20:33:59 +0800

    commit

--- msglist.py
+++ msglist.py
@@ -21,13 +21,21 @@ def msglist_Template():
         template=CarouselTemplate(
             columns=[
                 CarouselColumn(
-                    thumbnail_image_url='',
+                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Number_1_in_green_rounded_square.svg/200px-Number_1_in_green_rounded_square.svg.png',
                     title='這是第一塊模板',
                     text='一個模板可以有三個按鈕',
                     actions=[
+                        PostbackTemplateAction(
+                            label='',
+                            data=''
+                        ),
                         MessageTemplateAction(
                             label='用戶發送訊息',
                             text='我知道這是1'
+                        ),
+                        URITemplateAction(
+                            label='',
+                            uri=''
                         )
                     ]
                 ),
